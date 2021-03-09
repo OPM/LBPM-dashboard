0779f86dfc7e39961fd40a7e933c99d07ef9c413
James McClure <jemcclur@gmail.com>
2021-03-09 15:17:28 -0500

    updated color model

--- LBPM/views.py
+++ LBPM/views.py
@@ -15,18 +15,84 @@ def simulation(request):
    Nx = request.POST.get('Nx')
    Ny = request.POST.get('Ny')
    Nz = request.POST.get('Nz')
-   capillary_number = request.POST.get('capillary_number')
-   viscosity_ratio = request.POST.get('viscosity_ratio')
-   density_ratio = request.POST.get('density_ratio')
-   interfacial_tension = request.POST.get('interfacial_tension')
+   capillary_number = float(request.POST.get('capillary_number'))
+   viscosity_ratio = float(request.POST.get('viscosity_ratio'))
+   density_ratio = float(request.POST.get('density_ratio'))
+   interfacial_tension = float(request.POST.get('interfacial_tension'))
+   ift_units = request.POST.get('ift_units')
+   kinematic_viscosity_ratio = viscosity_ratio / density_ratio
+   tau = 0.7
+   nu = (tau-0.5)/3
+   flux = 0.0
+   BC = 0
+   alpha = 0.01
+   beta = 0.95
+   if ift_units == "LBM" :
+      alpha = interfacial_tension / 6.0
+   else :
+      alpha = 0.008
+   if kinematic_viscosity_ratio > 1.0 :
+      nu_w = nu
+      tau_w = tau
+      nu_n = nu_w*kinematic_viscosity_ratio
+      tau_n = 3*nu_n + 0.5
+   else :
+      nu_n = nu
+      tau_n = tau
+      nu_w = nu_n*kinematic_viscosity_ratio
+      tau_w = 3*nu_w + 0.5
+   if nu_w > 1.5 :
+      print("Warning: phase w kinematic viscosity too high!")
+      nu_w = 1.5
+   rho_w = 1.0
+   rho_n = rho_w*density_ratio
+   if protocol == "Steady-state relperm" :
+      Fx = 0.0
+      Fy = 0.0
+      Fz = 1.0e-5
+   elif protocol == "Centrifuge" :
+      Fx = 0.0
+      Fy = 0.0
+      Fz = 1.0e-5
+      BC = 3
+   elif protocol == "Core flooding" :
+      Fx = 0.0
+      Fy = 0.0
+      Fz = 0.0
+      flux = capillary_number*interfacial_tension/(rho_w*nu_w)
+      BC = 4
+   elif protocol ==  "Image sequence" :
+      Fx = 0.0
+      Fy = 0.0
+      Fz = 1.0e-5
    LBPM_input_file = "Domain {\n"
    LBPM_input_file += '   filename = "'+filename+'"'+"\n"
    LBPM_input_file += "   N = "+Nx+", "+Ny+", "+Nz+"\n"
    LBPM_input_file += "   n = "+Nx+", "+Ny+", "+Nz+"\n"
    LBPM_input_file += "   nproc = 1, 1, 1 \n"
+   LBPM_input_file += '   BC = '+str(BC)+"\n"
+   if protocol == "Steady-state relperm" :
+      LBPM_input_file += "   InletLayers = 0, 0, 5 \n"
+      LBPM_input_file += "   OutletLayers = 0, 0, 5 \n"
+   elif protocol == "Image sequence" :
+      LBPM_input_file += "   InletLayers = 0, 0, 5 \n"
+      LBPM_input_file += "   OutletLayers = 0, 0, 5 \n"
    LBPM_input_file += "}\n"
    LBPM_input_file += "Color {\n"   
-   LBPM_input_file += '   protocol = "'+protocol+'"'+"\n"
+   #LBPM_input_file += '   protocol = "'+protocol+'"'+"\n"
+   if protocol == "Steady-state relperm" :
+      LBPM_input_file += '   protocol = "shell aggregation"'+"\n"
+   elif protocol ==  "Image sequence" :
+      LBPM_input_file += '   protocol = "image sequence"'+"\n"
+   LBPM_input_file += '   rho_w = '+str(rho_w)+"\n"
+   LBPM_input_file += '   rho_n = '+str(rho_n)+"\n"
+   LBPM_input_file += '   tau_w = '+str(tau_w)+"\n"
+   LBPM_input_file += '   tau_n = '+str(tau_n)+"\n"
+   LBPM_input_file += '   alpha = '+str(alpha)+"\n"
+   LBPM_input_file += '   beta = '+str(beta)+"\n"
+   LBPM_input_file += "   F = "+str(Fx)+", "+str(Fy)+", "+str(Fz)+"\n"
+   if protocol == "Core flooding" :
+      LBPM_input_file += "   flux = "+str(flux)+"\n"
    LBPM_input_file += "}\n"
    LBPM_input_file += "Analysis {\n"
    LBPM_input_file += "}\n"
@@ -35,10 +101,10 @@ def simulation(request):
    print(LBPM_input_file)
    #imageFile.save()
    #Nx = form(request.POST['Nx'])
-   print("Capillary number = %s" % capillary_number)
-   print("Viscosity ratio = %s" % viscosity_ratio)
-   print("Density ratio = %s" % density_ratio)
-   print("interfacial tension = %s" % interfacial_tension)
+   print("Capillary number = %s" % str(capillary_number))
+   print("Viscosity ratio = %s" % str(viscosity_ratio))
+   print("Density ratio = %s" % str(density_ratio))
+   print("interfacial tension = %s" % str(interfacial_tension))
    return render(request, 'LBPM/simulation.html', {'inputfile':LBPM_input_file})
 
    #   return HttpResponse(LBPM_input_file)
