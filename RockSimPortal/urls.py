28af558f886128673d3910d9e958764c5057589f
James E McClure <mcclurej@vt.edu>
2021-07-21 10:24:38 -0400

    combine image upload with data set

--- RockSimPortal/urls.py
+++ RockSimPortal/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('project/', include('upload.urls')),
     path('LBPM/', include('LBPM.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('admin/', admin.site.urls),
-]
+    #path('media/','django.views.static.serve','media'),
+    #url(r'^media/$',media,name='media'),
+ ]
